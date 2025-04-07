9d0a9168d17f94986dc3edd3772dfa80746d042c
gjbm2 <github2@gregmarsh.co.uk>
2025-04-07 15:20:44 +0100

    Update rebuildall.sh

--- rebuildall.sh
+++ rebuildall.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
+
 source .venv/bin/activate
 
 set -o allexport
 source .env
 set +o allexport
 
-echo "{$DOCKERHUB_TOKEN}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
+# echo "{$DOCKERHUB_TOKEN}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
 
-docker build --build-arg MODEL_TYPE=base --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-base --platform linux/amd64 .
-docker push gjbm2/runpod-worker-comfy:dev-base
+DOCKER_CONFIG=$HOME/.docker-wsl docker build --build-arg MODEL_TYPE=wan2 --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-wan2 --platform linux/amd64 .
+DOCKER_CONFIG=$HOME/.docker-wsl docker push gjbm2/runpod-worker-comfy:dev-wan2
 
-docker build --build-arg MODEL_TYPE=flux1 --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-flux1 --platform linux/amd64 .
-docker push gjbm2/runpod-worker-comfy:dev-flux1
+DOCKER_CONFIG=$HOME/.docker-wsl docker build --build-arg MODEL_TYPE=base --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-base --platform linux/amd64 .
+DOCKER_CONFIG=$HOME/.docker-wsl docker push gjbm2/runpod-worker-comfy:dev-base
 
-docker build --build-arg MODEL_TYPE=sd35 --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-sd35 --platform linux/amd64 .
-docker push gjbm2/runpod-worker-comfy:dev-sd35
+DOCKER_CONFIG=$HOME/.docker-wsl docker build --build-arg MODEL_TYPE=flux1 --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-flux1 --platform linux/amd64 .
+DOCKER_CONFIG=$HOME/.docker-wsl docker push gjbm2/runpod-worker-comfy:dev-flux1
 
-docker build --build-arg MODEL_TYPE=sdxl --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-sdxl --platform linux/amd64 .
-docker push gjbm2/runpod-worker-comfy:dev-sdxl
+DOCKER_CONFIG=$HOME/.docker-wsl docker build --build-arg MODEL_TYPE=sd35 --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-sd35 --platform linux/amd64 .
+DOCKER_CONFIG=$HOME/.docker-wsl docker push gjbm2/runpod-worker-comfy:dev-sd35
+
+DOCKER_CONFIG=$HOME/.docker-wsl docker build --build-arg MODEL_TYPE=sdxl --build-arg HUGGINGFACE_ACCESS_TOKEN=${HUGGINGFACE_ACCESS_TOKEN} -t gjbm2/runpod-worker-comfy:dev-sdxl --platform linux/amd64 .
+DOCKER_CONFIG=$HOME/.docker-wsl docker push gjbm2/runpod-worker-comfy:dev-sdxl
