225fb897ac07c57b1f1577d9b0c814e75b804fee
gjbm2 <github2@gregmarsh.co.uk>
2025-04-12 02:49:04 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -6,39 +6,44 @@ echo
 echo "== CONTAINER INIT =="
 echo 
 
-# Patch image2video
-wget -O /comfyui/comfy_extras/image2video.py https://raw.githubusercontent.com/pftq/Wan2.1-Fixes/refs/heads/main/wan/image2video.py 
+if [ "$LIVE_PATCH" == "true" ]; then
+    # Patch image2video
+    wget -O /comfyui/comfy_extras/image2video.py https://raw.githubusercontent.com/pftq/Wan2.1-Fixes/refs/heads/main/wan/image2video.py 
 
-# Get laetest rp_handler script
-wget -O /rp_handler.py "https://raw.githubusercontent.com/gjbm2/runpod-worker-comfy/main/src/rp_handler.py?$(date +%s%N)" 
-wget -O /restore_snapshots.sh "https://raw.githubusercontent.com/gjbm2/runpod-worker-comfy/refs/heads/main/src/restore_snapshot.sh?$(date +%s%N)" 
-chmod +x /restore_snapshots.sh
+    # Get laetest rp_handler script
+    wget -O /rp_handler.py "https://raw.githubusercontent.com/gjbm2/runpod-worker-comfy/main/src/rp_handler.py?$(date +%s%N)" 
+    wget -O /restore_snapshots.sh "https://raw.githubusercontent.com/gjbm2/runpod-worker-comfy/refs/heads/main/src/restore_snapshot.sh?$(date +%s%N)" 
+    chmod +x /restore_snapshots.sh
+fi
 
 # Check we defintitely have the right version of rp_handler.py
 if [ "$DETAILED_COMFY_LOGGING" = "true" ]; then
     head -n 1 /rp_handler.py
 fi
 
-export AWS_ACCESS_KEY_ID="$BUCKET_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$BUCKET_SECRET_ACCESS_KEY"
-export AWS_DEFAULT_REGION="$BUCKET_AWS_REGION"
-aws s3 sync s3://stable-diffusion-bucket-gjbm2/models /runpod-volume/models --no-progress --delete
-aws s3 sync s3://stable-diffusion-bucket-gjbm2/custom_nodes /runpod-volume/custom_nodes --no-progress --delete
-aws s3 sync s3://stable-diffusion-bucket-gjbm2/snapshots /runpod-volume/snapshots --no-progress --delete
+if [ "$AWS_SYNC" == "true" ]; then
+    export AWS_ACCESS_KEY_ID="$BUCKET_ACCESS_KEY_ID"
+    export AWS_SECRET_ACCESS_KEY="$BUCKET_SECRET_ACCESS_KEY"
+    export AWS_DEFAULT_REGION="$BUCKET_AWS_REGION"
+    aws s3 sync s3://stable-diffusion-bucket-gjbm2/models /runpod-volume/models --no-progress --delete
+    aws s3 sync s3://stable-diffusion-bucket-gjbm2/custom_nodes /runpod-volume/custom_nodes --no-progress --delete
+    aws s3 sync s3://stable-diffusion-bucket-gjbm2/snapshots /runpod-volume/snapshots --no-progress --delete
+fi
 
 if [ "$COPY_SCRIPTS" == "true" ]; then
     # copy them over for performance reasons...
     cp -v -u -r /runpod-volume/models/* /comfyui/models/
 fi
 
-cp -v -u /runpod-volume/snapshots/* /
-
-# Try to restore nodes
-apt update && apt install -y libglib2.0-0        # TEMP UNTIL WE HAVE RE-ROLLED CONTAINER
-/restore_snapshots.sh
-
-# pull in custom nodes
-# ln -sf /runpod-volume/custom_nodes/* /comfyui/custom_nodes 
+if [ "$COPY_SNAPSHOTS" == "true" ]; then
+    cp -v -u /runpod-volume/snapshots/* 
+    # Try to restore nodes
+    # apt update && apt install -y libglib2.0-0        # TEMP UNTIL WE HAVE RE-ROLLED CONTAINER
+    /restore_snapshots.sh
+    
+    # pull in custom nodes
+    # ln -sf /runpod-volume/custom_nodes/* /comfyui/custom_nodes 
+fi
 
 # Use libtcmalloc for better memory management
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
