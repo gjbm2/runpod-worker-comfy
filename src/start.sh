ba63e462f90656bae01a7f35d0976e9b2ea43b6a
gjbm2 <github2@gregmarsh.co.uk>
2025-04-21 10:42:48 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -7,13 +7,37 @@ echo "== CONTAINER INIT =="
 echo 
 
 if [ "$LIVE_PATCH" == "true" ]; then
-    # Patch image2video
-    wget -O /comfyui/comfy_extras/image2video.py https://raw.githubusercontent.com/pftq/Wan2.1-Fixes/refs/heads/main/wan/image2video.py 
+    ## Patch image2video
+    #wget -O /comfyui/comfy_extras/image2video.py https://raw.githubusercontent.com/pftq/Wan2.1-Fixes/refs/heads/main/wan/image2video.py 
 
     # Get laetest rp_handler script
     wget -O /rp_handler.py "https://raw.githubusercontent.com/gjbm2/runpod-worker-comfy/main/src/rp_handler.py?$(date +%s%N)" 
     wget -O /restore_snapshots.sh "https://raw.githubusercontent.com/gjbm2/runpod-worker-comfy/refs/heads/main/src/restore_snapshot.sh?$(date +%s%N)" 
     chmod +x /restore_snapshots.sh
+
+    wget -nc -O /runpod-volume/models/diffusion_models/wan2.1_flf2v_720p_14B_fp16.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_flf2v_720p_14B_fp16.safetensors
+
+    # upgrade comfy
+    cd /comfyui
+    
+    # Clean any local changes to avoid conflicts
+    git reset --hard
+    git clean -fd
+    
+    # Fetch all tags
+    git fetch --all --tags
+    
+    # Checkout the specific version
+    git checkout v0.3.29
+    
+    # Pull the latest for that tag (just in case)
+    git pull origin v0.3.29
+    
+    # Reinstall dependencies (if needed)
+    pip install -r requirements.txt
+
+    wget 
+    
 fi
 
 # Check we defintitely have the right version of rp_handler.py
@@ -33,6 +57,7 @@ fi
 if [ "$COPY_MODELS" == "true" ]; then
     # copy them over for performance reasons...
     cp -v -u -r /runpod-volume/models/* /comfyui/models/
+    #cp -v -u  /runpod-volume/models/diffusion_models/[NAME HERE] /comfyui/models/diffusion_models/[NAME HERE]
 fi
 
 if [ "$COPY_SNAPSHOTS" == "true" ]; then
